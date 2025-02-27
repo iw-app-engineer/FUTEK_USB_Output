020ad33ab24d6b68283da2e5420baf31aeb17a50
iw-app-engineer <iwells@futek.com>
2025-02-27 10:40:01 -0800

    ASCII_OUTPUT.py

--- ASCII_OUTPUT.py
+++ ASCII_OUTPUT.py
@@ -1,1 +1,9 @@
+import serial
+
+ser = serial.Serial('COM5', 2000000)  # Adjust 'COM5' to your actual port, #USB225 has baud rate of 2000000
+
+while True:
+    if ser.in_waiting > 0:
+        data = ser.read(ser.in_waiting)
+        print(data)
 
